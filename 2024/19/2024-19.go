7e1e58e89b72818f03e4a6ad301a0f4f77565844
Martin BENDA <martin.benda@tipsport.cz>
2024-12-19 09:26:08 +0100

    2024/19 - trie

--- 2024/19/2024-19.go
+++ 2024/19/2024-19.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	. "github.com/bendiscz/aoc/aoc"
-	"strings"
 )
 
 const (
@@ -28,13 +27,55 @@ func main() {
 	Run(year, day, example, solve)
 }
 
+var index = []int{'b': 0, 'w': 1, 'u': 2, 'r': 3, 'g': 4}
+
+type trie struct {
+	term bool
+	n    [5]int
+}
+
+func buildTrie(patterns []string) []trie {
+	t := []trie{{}}
+	for _, p := range patterns {
+		ti := 0
+		for i := 0; i < len(p); i++ {
+			ch := index[p[i]]
+			ni := t[ti].n[ch]
+			if ni == 0 {
+				ni = len(t)
+				t[ti].n[ch] = ni
+				t = append(t, trie{})
+			}
+			ti = ni
+		}
+		t[ti].term = true
+	}
+	return t
+}
+
+func matchTrie(t []trie, towel string, lengths []int) []int {
+	for i, ti := 0, 0; ; i++ {
+		if t[ti].term {
+			lengths = append(lengths, i)
+		}
+		if i == len(towel) {
+			break
+		}
+		ti = t[ti].n[index[towel[i]]]
+		if ti == 0 {
+			break
+		}
+	}
+	return lengths
+}
+
 func solve(p *Problem) {
-	patterns := SplitFields(p.ReadLine())
+	t := buildTrie(SplitFields(p.ReadLine()))
 	p.NextLine()
 
 	s1, s2 := 0, 0
 	for p.NextLine() {
-		c := count(patterns, p.Line())
+		c := count(t, p.Line())
 		if c > 0 {
 			s1++
 		}
@@ -44,14 +85,13 @@ func solve(p *Problem) {
 	p.PartTwo(s2)
 }
 
-func count(patterns []string, towel string) int {
-	d := make([]int, len(towel)+1)
+func count(t []trie, towel string) int {
+	d, ls := make([]int, len(towel)+1), []int(nil)
 	d[0] = 1
 	for i := 0; i < len(towel); i++ {
-		for _, p := range patterns {
-			if strings.HasPrefix(towel[i:], p) {
-				d[i+len(p)] += d[i]
-			}
+		ls = matchTrie(t, towel[i:], ls[:0])
+		for _, l := range ls {
+			d[i+l] += d[i]
 		}
 	}
 	return d[len(d)-1]

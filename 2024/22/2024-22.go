e00170b5bbd33fa407e7b61e7b94ba7ffea9d08f
Martin BENDA <martin.benda@tipsport.cz>
2024-12-22 07:31:56 +0100

    2024/22 - cleanup, optimized

--- 2024/22/2024-22.go
+++ 2024/22/2024-22.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	. "github.com/bendiscz/aoc/aoc"
 )
 
@@ -24,109 +23,44 @@ func main() {
 const M = 16777216
 
 func next(x int) int {
-	x = x ^ (x << 6)
+	x ^= x << 6
 	x %= M
-	x = x ^ (x >> 5)
-	x %= M
-	x = x ^ (x << 11)
+	x ^= x >> 5
+	x ^= x << 11
 	x %= M
 	return x
 }
 
-func extract(s []int, d1, d2, d3, d4 int) int {
-	d := [4]int{s[1] - s[0], s[2] - s[1], s[3] - s[2], s[4] - s[3]}
-	for i := 4; i < len(s); i++ {
-		if i > 4 {
-			copy(d[:], d[1:])
-			d[3] = s[i] - s[i-1]
-		}
-
-		if d1 == d[0] && d2 == d[1] && d3 == d[2] && d4 == d[3] {
-			return s[i]
-		}
-	}
-	return 0
-}
-
 type diff [4]int
 
-type diffs struct {
-	m map[diff]int
-}
+func generateSecrets(sums map[diff]int, seed, n int) int {
+	x, prev, d := seed, seed%10, diff{}
+	seen := Set[diff]{}
 
-func makeDiffs(s []int) diffs {
-	ds := diffs{m: map[diff]int{}}
-	d := diff{0, s[1] - s[0], s[2] - s[1], s[3] - s[2]}
-	for i := 4; i < len(s); i++ {
+	for i := 0; i < n; i++ {
+		x = next(x)
+		b := x % 10
 		copy(d[:], d[1:])
-		d[3] = s[i] - s[i-1]
-		if _, ok := ds.m[d]; !ok {
-			ds.m[d] = s[i]
+		d[3], prev = b-prev, b
+		if i >= 4 && !seen.Contains(d) {
+			seen[d] = SET
+			sums[d] += b
 		}
 	}
-	return ds
-}
-
-func (ds diffs) extract(d0, d1, d2, d3 int) int {
-	return ds.m[diff{d0, d1, d2, d3}]
+	return x
 }
 
 func solve(p *Problem) {
-	s1, s2 := 0, 0
-
-	//g := grid{NewMatrix[cell](Rectangle(len(p.PeekLine()), 0))}
-
-	//s := p.ReadAll()
-
-	secrets := [][]int(nil)
+	s1, sums := 0, map[diff]int{}
 	for p.NextLine() {
-		x := ParseInt(p.Line())
-
-		s := []int{x % 10}
-		for i := 0; i < 2000; i++ {
-			x = next(x)
-			s = append(s, x%10)
-
-		}
-		secrets = append(secrets, s)
-		s1 += x
+		seed := ParseInt(p.Line())
+		s1 += generateSecrets(sums, seed, 2000)
 	}
 	p.PartOne(s1)
 
-	//testIt(secrets[1])
-
-	ds := make([]diffs, len(secrets))
-	for i, s := range secrets {
-		ds[i] = makeDiffs(s)
+	s2 := 0
+	for _, x := range sums {
+		s2 = max(s2, x)
 	}
-
-	best := 0
-	for d1 := -9; d1 <= 9; d1++ {
-		for d2 := -9; d2 <= 9; d2++ {
-			for d3 := -9; d3 <= 9; d3++ {
-				for d4 := -9; d4 <= 9; d4++ {
-					sum := 0
-					for _, d := range ds {
-						sum += d.extract(d1, d2, d3, d4)
-					}
-					best = max(best, sum)
-				}
-			}
-		}
-	}
-
-	s2 = best
-
 	p.PartTwo(s2)
-
-	//os.Exit(1)
-}
-
-func testIt(s []int) {
-	fmt.Println(len(s))
-	for i := 5; i < len(s); i++ {
-		if s[i] == 7 && s[i-3]-s[i-4] == -2 {
-			fmt.Printf("%v %d %d %d %d\n", s[i-4:i+1], s[i-3]-s[i-4], s[i-2]-s[i-3], s[i-1]-s[i-2], s[i-0]-s[i-1])
-		}
-	}
 }
